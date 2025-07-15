eef2f3be48e006cc9c91991dd7e9728cbbe48f66
Jay Nayon Jr. <Jay.Nayon@amcsgroup.com>
2025-07-15 19:39:40 +0800

    Add generic repository interface

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace StatSanctum.Repositories
+{
+    public interface IRepository<TDto, TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> Create(TDto dto);
+        Task<TEntity> GetById(int id);
+        Task DeleteById(int id);
+        Task<TEntity> Update(int id, TDto dto);
+    }
+}
