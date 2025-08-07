f04ba87f28a41bb694f9c64acda39d99ce24438a
Jaynayon <jay.nayonjr@cit.edu>
2025-08-07 11:05:57 +0800

    Create Helper folder; Create PassworldHelper

---
+++ src/StatSanctum.API/Helper/PasswordHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace StatSanctum.API.Helper
+{
+    public static class PasswordHelper
+    {
+        public static string GenerateSalt(int length = 16)
+        {
+            using var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[length];
+            rng.GetBytes(saltBytes);
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(salt))
+            {
+                throw new ArgumentException("Password and salt cannot be null or empty.");
+            }
+            using var sha256 = SHA256.Create();
+            var saltedPassword = $"{password}{salt}";
+            var hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
